a7b28e6ffebd6e7f26dc7aad0a41cef33623fe32
Medha <11g17medhagupta@gmail.com>
2025-01-23 22:19:05 +0530

    first commit

---
+++ frontend/src/assets/data/doctors.js
@@ -0,0 +1,36 @@
+import doctorImg01 from "../images/doctor-img01.png";
+import doctorImg02 from "../images/doctor-img02.png";
+import doctorImg03 from "../images/doctor-img03.png";
+
+export const doctors = [
+  {
+    id: "01",
+    name: "Dr. Jacob Anders",
+    specialization: "Surgeon",
+    avgRating: 4.9,
+    totalRating: 272,
+    photo: doctorImg01,
+    totalPatients: 1500,
+    hospital: "CityCare Medical Facility, New York.",
+  },
+  {
+    id: "02",
+    name: "Dr. Matthew Hale",
+    specialization: "Neurologist",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: doctorImg02,
+    totalPatients: 1500,
+    hospital: "CityCare Medical Facility, New York.",
+  },
+  {
+    id: "03",
+    name: "Dr. Oliver Brooks",
+    specialization: "Dermatologist",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: doctorImg03,
+    totalPatients: 1500,
+    hospital: "CityCare Medical Facility, New York.",
+  },
+];
